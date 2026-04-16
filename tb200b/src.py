973222f12cd6fb2fe83146aa1214cbe2277fe100
max.kerker <maximilian.lkp@gmail.com>
2026-04-16 14:34:29 +0200

    Added Functionality, still in testing mode

--- tb200b/src.py
+++ tb200b/src.py
@@ -34,7 +34,7 @@ class TB200B:
     def set_passivemode(self) -> None:
         self.__write_command(self.commands["command_2"])
     
-    def get_sensorparameters(self, answer):
+    def get_sensorparameters(self, answer) -> dict:
         #self.__write_command(self.commands["command_3"])
         #response = self.__read_response()
         response = answer
@@ -43,21 +43,28 @@ class TB200B:
         maximum_range = (response[1] << 8) | response[2]
         unit = units_inverse[response[3]]
         decimals = (response[7]) >> 4
-        decimals = response[7] & 0b11110000
         sign = response[7] & 0b00001111
         if sign == 0:
             sign = 1
         elif sign == 1:
             sign = -1
-        sensor_info = {"Type": sensor, "Max Range": maximum_range, "Unit": unit, "Sign": sign, "Decimals": decimals}
+        sensor_info = {"type": sensor, "maxrange": maximum_range, "unit": unit, "sign": sign, "decimals": decimals}
         return sensor_info
 
-    def get_concentration(self):
-        self.__write_command(self.commands["command_5"])
-        response = self.__read_response()
+    def get_combinedread(self, answer) -> dict:
+        #self.__write_command(self.commands["command_5"])
+        #response = self.__read_response()
+        response = answer
+        conc = float(response[2]) * 256 + float(response[3])
+        temp = float((response[8] << 8) | response[9]) / 100
+        hum = float((response[10] << 8) | response[11]) / 100
+        combined = {"conc": conc, "temp": temp, "hum": hum}
+        return combined
+
 
 tb200b = TB200B("COM2")
-print(tb200b.get_sensorparameters(b'\x22\x07\xD0\x02\x00\x00\x00\x3a\x35'))
+print(tb200b.get_sensorparameters(b'\x18\x00\xc8\x02\x00\x00\x00\x01\x35'))
+print(tb200b.get_combinedread(b'\xFF\x87\x00\x2a\x03\xE8\x00\x20\x09\xc4\x13\x88\xdc'))
 
 #print(units_inverse[0x08])
 #print(dict((v, k) for k, v in units.items()))

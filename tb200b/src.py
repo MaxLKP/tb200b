535c835dfe3e961ca6bdd3e79feb233d519e7c66
max.kerker <maximilian.lkp@gmail.com>
2026-06-04 11:12:04 +0200

    put testing in testing branch

--- tb200b/src.py
+++ tb200b/src.py
@@ -8,7 +8,7 @@ from tb200b.header import *
 class TB200B:
     def __init__(self, port):
         self.port = port
-        #self.serial = serial.Serial(port, baudrate = BAUDRATE, parity = PARITY, bytesize = BYTESIZE, stopbits = STOPBITS, rtscts = RTSCTS, timeout = TIMEOUT)
+        self.serial = serial.Serial(port, baudrate = BAUDRATE, parity = PARITY, bytesize = BYTESIZE, stopbits = STOPBITS, rtscts = RTSCTS, timeout = TIMEOUT)
         self.commands = COMMANDS
     
     def __write_command(self, command: bytes) -> None:
@@ -26,10 +26,9 @@ class TB200B:
     def set_passivemode(self) -> None:
         self.__write_command(self.commands["command_2"])
     
-    def get_sensorparameters(self, answer) -> dict:
-        #self.__write_command(self.commands["command_3"])
-        #response = self.__read_response()
-        response = answer
+    def get_sensorparameters(self) -> dict:
+        self.__write_command(self.commands["command_3"])
+        response = self.__read_response()
         sensor = SENSOR_TYPES_INVERSE[response[0]]
         maximum_range = (response[1] << 8) | response[2]
         unit = UNITS_INVERSE[response[3]]
@@ -42,20 +41,11 @@ class TB200B:
         sensor_info = {"type": sensor, "maxrange": maximum_range, "unit": unit, "sign": sign, "decimals": decimals}
         return sensor_info
 
-    def get_combinedread(self, answer) -> dict:
-        #self.__write_command(self.commands["command_5"])
-        #response = self.__read_response()
-        response = answer
+    def get_combinedread(self) -> dict:
+        self.__write_command(self.commands["command_5"])
+        response = self.__read_response()
         conc = float(response[2]) * 256 + float(response[3])
         temp = float((response[8] << 8) | response[9]) / 100
         hum = float((response[10] << 8) | response[11]) / 100
         combined = {"conc": conc, "temp": temp, "hum": hum}
         return combined
-
-
-tb200b = TB200B("COM2")
-print(tb200b.get_sensorparameters(b'\x18\x00\xc8\x02\x00\x00\x00\x01\x35'))
-print(tb200b.get_combinedread(b'\xFF\x87\x00\x2a\x03\xE8\x00\x20\x09\xc4\x13\x88\xdc'))
-
-#print(units_inverse[0x08])
-#print(dict((v, k) for k, v in units.items()))

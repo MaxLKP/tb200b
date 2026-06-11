e7a02c06f27426fa5686c41a5b638b276f53ad01
max.kerker <maximilian.lkp@gmail.com>
2026-06-11 21:48:34 +0200

    Done some tests on readout

--- tb200b/src.py
+++ tb200b/src.py
@@ -46,15 +46,17 @@ class TB200B:
         #self.__write_command(self.commands["command_5"])
         #response = self.__read_response()
         response = answer
-        conc = float(response[2]) * 256 + float(response[3])
+        conc_mgm3 = float(response[2]) * 256 + float(response[3])
+        conc_ppm = float(response[6]) * 256 + float(response[7])
         temp = float((response[8] << 8) | response[9]) / 100
         hum = float((response[10] << 8) | response[11]) / 100
-        combined = {"conc": conc, "temp": temp, "hum": hum}
+        combined = {"conc": conc_mgm3, "temp": temp, "hum": hum}
+        print(conc_mgm3, conc_ppm)
         return combined
 
 tb200b = TB200B("COM2")
-print(tb200b.get_sensorparameters(b'\x18\x00\xc8\x02\x00\x00\x00\x01\x35'))
-print(tb200b.get_combinedread(b'\xFF\x87\x00\x2a\x03\xE8\x00\x20\x09\xc4\x13\x88\xdc'))
-
+#print(tb200b.get_sensorparameters(b'\x18\x00\xc8\x02\x00\x00\x00\x01\x35'))
+#print(tb200b.get_combinedread(b'\xFF\x87\x00\x2a\x03\xE8\x00\x20\x09\xc4\x13\x88\xdc'))
+tb200b.get_combinedread(b'\xff\x87\x05\x88\x00\n\x06\xd5\x08\xa2\x0f9\x15')
 #print(units_inverse[0x08])
 #print(dict((v, k) for k, v in units.items()))

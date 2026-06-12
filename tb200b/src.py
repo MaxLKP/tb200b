fbed338eee7122086359f151a9689424947080b2
max.kerker <maximilian.lkp@gmail.com>
2026-06-12 15:50:22 +0200

    Added checksum test for responses.

--- tb200b/src.py
+++ tb200b/src.py
@@ -21,6 +21,13 @@ class TB200B:
         time.sleep(0.1)
         return response
 
+    def __checksum(self, response) -> int:
+        checksum_response = (~sum(response[1:-1]) & 0xff) + 1 # According to communication protocol
+        if checksum_response == response[-1]:
+            return 1
+        else:
+            return 0
+
     def set_activemode(self) -> None:
         self.__write_command(self.commands["command_1"])
     
@@ -30,34 +37,46 @@ class TB200B:
     def get_sensorparameters(self) -> dict:
         self.__write_command(self.commands["command_3"])
         response = self.__read_response(9)
-        sensor = SENSOR_TYPES_INVERSE[response[0]]
-        maximum_range = (response[1] << 8) | response[2]
-        unit = UNITS_INVERSE[response[3]]
-        decimals = (response[7]) >> 4
-        sign = response[7] & 0b00001111
-        if sign == 0:
-            sign = 1
-        elif sign == 1:
-            sign = -1
-        sensor_info = {"type": sensor, "maxrange": maximum_range, "unit": unit, "sign": sign, "decimals": decimals}
+        checksum = self.__checksum(response)
+        if checksum == 1:
+            sensor = SENSOR_TYPES_INVERSE[response[0]]
+            maximum_range = (response[1] << 8) | response[2]
+            unit = UNITS_INVERSE[response[3]]
+            decimals = (response[7]) >> 4
+            sign = response[7] & 0b00001111
+            if sign == 0:
+                sign = 1
+            elif sign == 1:
+                sign = -1
+            sensor_info = {"type": sensor, "maxrange": maximum_range, "unit": unit, "sign": sign, "decimals": decimals}
+        elif checksum == 0:
+            sensor_info = {"type": -1., "maxrange": -1., "unit": -1., "sign": -1., "decimals": -1.}
         return sensor_info
 
     def get_combinedread(self) -> dict:
         self.__write_command(self.commands["command_6"])
         response = self.__read_response(13)
         decimals = self.parameters["decimals"]
-        conc_mgm3 = (float(response[2]) * 256 + float(response[3])) / 10**decimals 
-        conc_ppm = (float(response[6]) * 256 + float(response[7])) / 10**decimals
-        temp = float((response[8] << 8) | response[9]) / 100
-        hum = float((response[10] << 8) | response[11]) / 100
-        combined = {"conc_mgm3": conc_mgm3, "conc_ppm": conc_ppm, "temp": temp, "hum": hum}
+        checksum = self.__checksum(response)
+        if checksum == 1:
+            conc_mgm3 = (float(response[2]) * 256 + float(response[3])) / 10**decimals 
+            conc_ppm = (float(response[6]) * 256 + float(response[7])) / 10**decimals
+            temp = float((response[8] << 8) | response[9]) / 100
+            hum = float((response[10] << 8) | response[11]) / 100
+            combined = {"conc_mgm3": conc_mgm3, "conc_ppm": conc_ppm, "temp": temp, "hum": hum}
+        elif checksum == 0:
+            combined = {"conc_mgm3": -1., "conc_ppm": -1., "temp": -1., "hum": -1.}
         return combined
         
     def get_concentration(self) -> dict:
         self.__write_command(self.commands["command_5"])
         response = self.__read_response(9)
         decimals = self.parameters["decimals"]
-        conc_mgm3 = (response[2] * 256 + response[3]) / 10**decimals
-        conc_ppm = (response[6] * 256 + response[7]) / 10**decimals
-        concentration = {"conc_mgm3": conc_mgm3, "conc_ppm": conc_ppm}
+        checksum = self.__checksum(response)
+        if checksum == 1:
+            conc_mgm3 = (response[2] * 256 + response[3]) / 10**decimals
+            conc_ppm = (response[6] * 256 + response[7]) / 10**decimals
+            concentration = {"conc_mgm3": conc_mgm3, "conc_ppm": conc_ppm}
+        elif checksum == 0:
+            concentration = {"conc_mgm3": -1., "conc_ppm": -1.}
         return concentration
